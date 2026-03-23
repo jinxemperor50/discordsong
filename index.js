71ce77f1634d662d8f7efb466ddc15d8bd8c06b9
jinxemperor50 <jinxemperor@gmail.com>
2026-03-24 00:58:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
 const { Client, GatewayIntentBits } = require('discord.js');
+const { joinVoiceChannel, createAudioPlayer, createAudioResource } = require('@discordjs/voice');
+const play = require('play-dl');
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildVoiceStates
+  ]
 });
 
 client.once('ready', () => {
   console.log(`Bot aktif sebagai ${client.user.tag}`);
+});
+
+client.on('messageCreate', async (message) => {
+  if (message.content.startsWith('!play')) {
+
+    const args = message.content.split(' ');
+    const url = args[1];
+
+    if (!url) return message.reply('Masukkan link YouTube!');
+
+    const voiceChannel = message.member.voice.channel;
+    if (!voiceChannel) return message.reply('Masuk voice channel dulu!');
+
+    const connection = joinVoiceChannel({
+      channelId: voiceChannel.id,
+      guildId: message.guild.id,
+      adapterCreator: message.guild.voiceAdapterCreator,
+    });
+
+    const stream = await play.stream(url);
+    const resource = createAudioResource(stream.stream, {
+      inputType: stream.type
+    });
+
+    const player = createAudioPlayer();
+    player.play(resource);
+    connection.subscribe(player);
+
+    message.reply('Memutar lagu 🎶');
+  }
 });
 
 client.login(process.env.TOKEN);
